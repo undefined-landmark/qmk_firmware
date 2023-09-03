5844463fa862b4145c91bd75b860e30c13e8e001
bas <basbasbas@protonmail.com>
2023-09-03 11:16:53 +0200

    add vmware shortcuts for windows

--- keyboards/lily58/keymaps/undefined-landmark/keymap.c
+++ keyboards/lily58/keymaps/undefined-landmark/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_RAISE] = LAYOUT(
   _______, KC_F1,  KC_F2,    KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-  _______, _______, _______, _______, _______, _______,                     _______, KC_PGDN, KC_PGUP, _______, KC_MPLY, KC_F12,
+  _______, _______, _______, _______, LCTL(LSFT(KC_F10)), _______,      LSFT(KC_F10), KC_PGDN, KC_PGUP, LCTL(LALT(KC_INS)), KC_MPLY, KC_F12,
   _______, _______, _______, _______, _______, _______,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
   _______, _______, _______, _______, _______, _______,  _______, _______,  _______, KC_VOLD, KC_VOLU, _______, KC_BSLS, _______,
                              _______, _______, _______,  _______, _______,  _______, _______, _______
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_LOWER] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______,_______, _______, _______,
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+  KC_F1,  LALT(KC_F4), KC_F3, KC_F4, KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
   KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
   _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
                              _______, _______, _______, _______, _______,  _______, _______, _______
